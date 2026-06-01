b674d3c9b23c821115044d95c4a4252f576566da
ClaraBaietti <cbaietti@udesa.edu.ar>
2026-06-01 14:22:20 -0300

    creacion de archivos para codigo

---
+++ datos.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 14:02:15 2026
+
+@author: clarabaietti
+"""
+
---
+++ recomendador.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 14:18:31 2026
+
+@author: clarabaietti
+"""
+
