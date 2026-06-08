ac7431e03ec3ef026094aba64187d28de97968e7
ClaraBaietti <cbaietti@udesa.edu.ar>
2026-06-08 12:26:51 -0300

    archivos cambiados

--- src/cuestionario.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun  1 14:17:32 2026
-
-@author: clarabaietti
-"""
-
--- src/recomendador.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun  1 14:18:31 2026
-
-@author: clarabaietti
-"""
-
