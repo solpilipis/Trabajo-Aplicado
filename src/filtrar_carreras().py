61947f9a06ee65cfd9b5696d6f1b5e4a572d5b58
solpilipis <spilipis@udesa.edu.ar>
2026-06-09 19:48:10 -0300

    Update filtrar_carreras().py

--- src/filtrar_carreras().py
+++ src/filtrar_carreras().py
@@ -10,13 +10,13 @@ def filtrar_carreras(df_carreras):
     provincia = input("Ingrese la provincia de interés: ").strip()
     tipo = input("Ingrese el tipo de universidad (Pública/Privada): ").strip()
     
-    # Aplicar máscaras booleanas (filtros dinámicos) con pandas
-    # Usamos .str.casefold() o .str.lower() para evitar problemas con mayúsculas/minúsculas
+    # Aplicar mascaras booleanas con pandas
+    # Usamos .str.casefold() o .str.lower() para evitar problemas con mayusculas/minusculas
     condicion_provincia = df_carreras['Provincia'].str.lower() == provincia.lower()
     condicion_tipo = df_carreras['Tipo'].str.lower() == tipo.lower()
     
-    # 3. Filtrar el DataFrame original
+    # Filtrar el DataFrame original
     df_filtrado = df_carreras[condicion_provincia & condicion_tipo]
     
-    # 4. Devolver el DataFrame resultante
+    # Devolver el DataFrame resultante
     return df_filtrado
