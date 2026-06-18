60b39e0fab7db0b77f075828eae32325851a8ccf
OliSalmoyraghi <osalmoyraghi@udesa.edu.ar>
2026-06-18 14:59:43 -0300

    docstring de visualizaciones.

--- src/visualizaciones.py
+++ src/visualizaciones.py
@@ -2,11 +2,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def mostrar_ranking(df_ranking, df_filtrado):
-
-    '''
+    """
     Muestra las 5 carreras recomendadas y permite
     al usuario elegir una para obtener más información.
-    '''
+
+    Parameters
+    ----------
+    df_ranking : pandas.Dataframe
+        Tabla que ocntiene el listado resumido de las 5 carreras recomendadas. 
+    df_filtrado : pandas.Dataframe
+        Tabla completa con las opciones académicas filtradas previamente según las 
+        preferencias del usuario (localidad, tipo de gestión, tipo de título y duración máxima).
+
+    Raises
+    ------
+    ValueError
+        Si el usuario ingresa un texto en lugar de un número, o si el número seleccionado 
+        está fuera del rango válido de opciones (menor a 1 o mayor al tamaño del ranking).
+
+    Returns
+    -------
+    None.
+    La función no retorna ningún valor; imprime los datos directamente en la terminal.
+
+    """
+
+    
     print()
     print("\n 🏅TOP 5 CARRERAS RECOMENDADAS 🏅 \n") 
     print()
@@ -68,7 +89,7 @@ def mostrar_perfil(resultados):
     Returns
     -------
     None.
-        La función no retrona ningún valor, dibuja el gráfico dierctamente en streamlit.  
+        La función no retorna ningún valor; procesa los datos y abre una ventana emergente conel gráfico.
 
     """
 
@@ -79,7 +100,7 @@ def mostrar_perfil(resultados):
    
     fig, ax = plt.subplots(figsize=(8, 4))
     
-    sns.barplot(x=dimensiones, y=puntajes, palette="Blues_d", ax=ax)
+    sns.barplot(x=dimensiones, y=puntajes, color="0f8b5d", ax=ax)
     
     ax.set_title("Puntajes por Dimensión", fontsize=12, fontweight='bold')
     ax.set_ylabel("Puntaje")
@@ -96,22 +117,24 @@ def mostrar_perfil(resultados):
 
 def mostrar_grafico_top5(top_5_filtrado):
     """
-    Dibuja un gráfico que compara la duración estimada de las carreras recomendadas. Muestra visualemente el tiempo mínimo base y la variación de años extras según la universidad.  
+    Dibuja un gráfico que compara la duración estimada de las carreras recomendadas. 
+    Muestra visualemente el tiempo mínimo base y la variación de años extras según la universidad.  
 
     Parameters
     ----------
-    top_5_filtrado : 
-       DESCRIPTION
+    top_5_filtrado : pandas.Dataframe
+       Una tabal de datos de las carreras recomendadas. 
 
     Raises
     ------
     ValueError
-        DESCRIPTION.
+        Si el DataFrame es None o está vacío, interrumpiendo 
+        la ejecución para evitar fallos en el dibujado.  
 
     Returns
     -------
     None.
-        La función no retrona ningún valor, dibuja el gráfico dierctamente en streamlit.  
+        La función no retorna ningún valor; procesa los datos y abre una ventana emergente conel gráfico.  
 
     """
     if top_5_filtrado is None or top_5_filtrado.empty:
@@ -129,9 +152,9 @@ def mostrar_grafico_top5(top_5_filtrado):
     df_rangos['variacion'] = df_rangos['max'] - df_rangos['min']
     
     fig, ax = plt.subplots(figsize=(9, 3.5))
-    ax.barh(y=df_rangos['Título'], width=df_rangos['min'], left=0, color='#4a90e2', label='Duración Mínima')
+    ax.barh(y=df_rangos['Título'], width=df_rangos['min'], left=0, color='#0f8b5d', label='Duración Mínima')
     ax.barh(y=df_rangos['Título'], width=df_rangos['variacion'], left=df_rangos['min'], 
-            color='#f5a623', alpha=0.8, edgecolor='black', linestyle='--', label='Variación según Universidad')
+            color='#f3b0b3', alpha=0.8, edgecolor='black', linestyle='--', label='Variación según Universidad')
     
     ax.set_xlim(0, df_rangos['max'].max() + 1)
     ax.set_xlabel("Años")
